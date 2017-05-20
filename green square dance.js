0942fec5d4863232b1b5c309c9518727393fb36e
Scott <sconn0088@gmail.com>
2017-05-20 14:31:40 -0400

    Add files via upload
    change between green and blue colors

--- green square dance.js
+++ green square dance.js
@@ -4,6 +4,14 @@ var func = function() {
 	$("#greensquare").animate({"top": "0%"}, 1500)
 	$("#greensquare").animate({"left": "0%"}, 2000)
 	setTimeout(func, 1000);
+	
+	var colors = ["green", "red", "purple", "blue"];
+	for(var i = 0; i < colors.length; i++) {
+		$("#greensquare").css("background-color", colors[i]);
+		setTimeout(function() {
+			$("#greensquare").css("background-color", colors[i%4]);
+		}, 2500);
+	}
 }
 
 func();
